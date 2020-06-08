479adbe118fb35dbdd178a856a0e1a3c56da439e
LouisBroucke <louis.broucke@outlook.com>
2020-06-08 10:22:18 +0200

    Add project files.

---
+++ ViewComponents/wwwroot/ViewComponents/Reclame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponents.wwwroot.ViewComponents
+{
+    public class Reclame : ViewComponent
+    {
+        static string[] powerbars = { "mars", "bounty", "snickers", "twix" };
+
+        public IViewComponentResult Invoke()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 4);
+            return View((object)powerbars[randomNumber]);
+        }
+    }
+}
