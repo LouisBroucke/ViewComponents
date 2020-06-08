99d852df20b1be4e5d11346fc495d6bc555c0df0
LouisBroucke <louis.broucke@outlook.com>
2020-06-08 11:28:46 +0200

    Fixed error hoofdstuk 8

--- ViewComponents/ViewComponents/Reclame.cs
+++ ViewComponents/ViewComponents/Reclame.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 
 namespace ViewComponents.wwwroot.ViewComponents
 {
@@ -15,6 +17,8 @@ namespace ViewComponents.wwwroot.ViewComponents
             Random random = new Random();
             int randomNumber = random.Next(0, 4);
             return View((object)powerbars[randomNumber]);
+            //return new ContentViewComponentResult("Deze tekst zal <letterlijk> worden weergegeven.");
+            //return new HtmlContentViewComponentResult(new HtmlString("Deze tekst zal <letterlijk> worden weergegeven."));
         }
     }
 }
